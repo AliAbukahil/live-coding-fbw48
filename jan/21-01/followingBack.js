1459935a8aa5e205ea99279113013268a62db186
ali.abukahil <ali.abukahil@gmail.com>
2021-01-21 20:29:38 +0100

    In Valley of The Shadows

---
+++ jan/21-01/followingBack.js
@@ -0,0 +1,29 @@
+let text = "I love Coder Ali AbuKahil";
+text = text.toUpperCase();
+console.log(text);
+text = text.toLowerCase();
+console.log(text);
+text;
+
+let firstName = "Coder";
+let lastName = " Abukahil";
+let name1 = firstName + lastName.concat();
+console.log(name1);
+
+let text1 = "I hate Lockdown 2.0";
+text1 = text1.toUpperCase();
+console.log(text1);
+text1 = text1.toLowerCase();
+console.log(text1);
+
+let firstPara = "   As I walk through the valley of The Shadows";
+
+let secondPara = ", I shall fear no evil";
+
+let thirdPara = ", because Allah is my shepherd!    ";
+
+let theGathering = firstPara + secondPara + thirdPara.concat();
+console.log(theGathering);
+
+theGathering = theGathering.trim();
+console.log(theGathering);
