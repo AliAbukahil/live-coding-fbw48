d040ee4091d211c16c9ed2c3a262e28b0678a483
ali.abukahil <ali.abukahil@gmail.com>
2021-01-21 21:43:23 +0100

    In Valley of The Shadows vol2

--- jan/21-01/followingBack.js
+++ jan/21-01/followingBack.js
@@ -27,3 +27,39 @@ console.log(theGathering);
 
 theGathering = theGathering.trim();
 console.log(theGathering);
+
+let index = thirdPara.charAt(10);
+console.log(index);
+
+let somText = "Have you seen the boogeyman? ";
+let length = somText.length;
+console.log(somText + " " + length);
+
+let indexOf = somText.indexOf("boogeyman");
+console.log(indexOf);
+
+indexOf = somText.indexOf("Have");
+console.log(indexOf);
+
+indexOf = somText.indexOf("seen");
+console.log(indexOf);
+
+indexOf = somText.indexOf("u");
+console.log(indexOf);
+
+indexOf = somText.indexOf("Natasha");
+console.log(indexOf);
+
+let somText3 = ["Where", "is", "The", "Wizard", "of", "Oz", "?"];
+
+somText3 = somText3.lastIndexOf("Oz");
+console.log(somText3);
+
+let search = somText.search("seen");
+console.log(search);
+
+let startWith = somText.startsWith("Have");
+console.log(startWith);
+
+startWith = somText.endsWith("?");
+console.log(startWith);
