b7733630ee1d845d2cd2128a2b7a8989045cef02
ali.abukahil <ali.abukahil@gmail.com>
2021-01-23 17:25:20 +0100

    created new files

---
+++ jan/21-01/weekendLS.js
@@ -0,0 +1,32 @@
+// Logical Operators
+
+//1 && and
+//2 || or
+//3 !  not
+
+let a = true;
+let b = false;
+
+if (a == true && b == false) {
+  // alert ("Condition has been verified");
+} else {
+  // alert ("Condition has not been verified");
+}
+
+// Switch statements :
+
+let fruit = "Orange";
+switch (fruit) {
+  case "Banana":
+    console.log("Banana Price 1$");
+    break;
+  case "Apple":
+    console.log("Apple Price is 2,99$");
+    break;
+  case "Strawberries":
+    console.log("1 Box Strawberries costs 5,99$");
+    break;
+  default:
+    console.log("Sorry We Don't Have This Kind Of Fruit!");
+    break;
+}
